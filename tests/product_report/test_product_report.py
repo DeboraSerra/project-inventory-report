810ad37dcfb3d302e144c0cf0ce8bbfd1cd12b60
debora <debora.r.serra@gmail.com>
2022-10-28 15:58:14 -0300

    feat: add req2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,16 @@
-# from inventory_report.inventory.product import Product
+from datetime import datetime
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    id = 1
+    nome_produto = 'produto1'
+    nome_empresa = 'empresa1'
+    fabricacao = datetime.today()
+    validade = datetime(2024, 10, 28)
+    n_de_serie = 'PR1'
+    armazenamento = 'em geladeira'
+    product = Product(id, nome_produto, nome_empresa, fabricacao, validade, n_de_serie, armazenamento)
+    expected = f'O produto {nome_produto} fabricado em {str(fabricacao)} por {nome_empresa} com validade até {str(validade)} precisa ser armazenado {armazenamento}.'
+
+    assert str(product) == expected
