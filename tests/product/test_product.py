0cbcabf9a0292997be137269942bdb9d46472efb
debora <debora.r.serra@gmail.com>
2022-10-28 15:54:17 -0300

    feat: add req1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from datetime import datetime
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    id = 1
+    nome_produto = 'produto1'
+    nome_empresa = 'empresa1'
+    fabricacao = datetime.today()
+    validade = datetime(2024, 10, 28)
+    n_de_serie = 'PR1'
+    armazenamento = 'em geladeira'
+    product = Product(id, nome_produto, nome_empresa, fabricacao, validade, n_de_serie, armazenamento)
+
+    assert product.id == id
+    assert product.nome_do_produto == nome_produto
+    assert product.nome_da_empresa == nome_empresa
+    assert product.data_de_fabricacao == str(fabricacao)
+    assert product.data_de_validade == str(validade)
+    assert product.numero_de_serie == n_de_serie
+    assert product.instrucoes_de_armazenamento == armazenamento
