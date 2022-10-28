c44658d68a8f94e04134addd216b87bd9937927d
debora <debora.r.serra@gmail.com>
2022-10-28 18:08:31 -0300

    feat: add req7

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xmltodict
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -8,31 +9,35 @@ def import_csv(path, type):
     with open(path) as file:
       raw_content = csv.DictReader(file)
       content = [item for item in raw_content]
-      if type == 'simples':
-          report = SimpleReport.generate(content)
-      elif type == 'completo':
-          report = CompleteReport.generate(content)
-      else:
-          raise TypeError
-      return report
+      return content
 
 
 def import_json(path, type):
     with open(path) as file:
       raw_content = json.load(file)
       content = [item for item in raw_content]
-      if type == 'simples':
-          report = SimpleReport.generate(content)
-      elif type == 'completo':
-          report = CompleteReport.generate(content)
-      else:
-          raise TypeError
-      return report
+      return content
+
+
+def import_xml(path, type):
+    with open(path) as file:
+        raw_content = file.read()
+        content = [item for item in xmltodict.parse(raw_content)['dataset']['record']]
+        return content
 
 class Inventory:
     @staticmethod
     def import_data(path, type):
         if '.csv' in path:
-            return import_csv(path, type)
+            content = import_csv(path, type)
         elif '.json' in path:
-            return import_json(path, type)
+            content = import_json(path, type)
+        else:
+            content = import_xml(path, type)
+        if type == 'simples':
+          report = SimpleReport.generate(content)
+        elif type == 'completo':
+            report = CompleteReport.generate(content)
+        else:
+            raise TypeError
+        return report
