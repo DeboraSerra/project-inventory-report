594566bb79b6afab99a8a0751ec8801ecfe8306c
debora <debora.r.serra@gmail.com>
2022-10-28 17:55:38 -0300

    feat: add req6

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,19 +1,38 @@
 import csv
+import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
 
+def import_csv(path, type):
+    with open(path) as file:
+      raw_content = csv.DictReader(file)
+      content = [item for item in raw_content]
+      if type == 'simples':
+          report = SimpleReport.generate(content)
+      elif type == 'completo':
+          report = CompleteReport.generate(content)
+      else:
+          raise TypeError
+      return report
+
+
+def import_json(path, type):
+    with open(path) as file:
+      raw_content = json.load(file)
+      content = [item for item in raw_content]
+      if type == 'simples':
+          report = SimpleReport.generate(content)
+      elif type == 'completo':
+          report = CompleteReport.generate(content)
+      else:
+          raise TypeError
+      return report
+
 class Inventory:
     @staticmethod
     def import_data(path, type):
-        with open(path) as file:
-            raw_content = csv.DictReader(file)
-            content = [item for item in raw_content]
-            print(content)
-            if type == 'simples':
-                report = SimpleReport.generate(content)
-            elif type == 'completo':
-                report = CompleteReport.generate(content)
-            else:
-                raise TypeError
-            return report
+        if '.csv' in path:
+            return import_csv(path, type)
+        elif '.json' in path:
+            return import_json(path, type)
